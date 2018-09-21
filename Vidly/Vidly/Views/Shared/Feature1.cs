98ee895f93a88e1a2cb7aeac40ccd40e63c46b95
RyanLim86 <30644324+RyanLim86@users.noreply.github.com>
2018-09-22 03:14:33 +0800

    Added Add  Method

--- Vidly/Vidly/Views/Shared/Feature1.cs
+++ Vidly/Vidly/Views/Shared/Feature1.cs
@@ -7,5 +7,13 @@ namespace Vidly.Views.Shared
 {
     public class Feature1
     {
+        public int add()
+        {
+            int x1 = 1;
+            int x2 = 2;
+            int sum = x1 + x2;
+            return sum;
+            
+        }
     }
 }
